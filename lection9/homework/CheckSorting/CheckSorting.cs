f614e11b8aa4a2e97c4ed68fa1e2a76780c36402
Олег Архипов <grimglance@mail.ru>
2023-12-06 02:39:42 +0700

    test

--- lection9/homework/CheckSorting/CheckSorting.cs
+++ lection9/homework/CheckSorting/CheckSorting.cs
@@ -7,7 +7,7 @@ internal class CheckSorting
         int[] array1 = { 10, 9, 8, 7, 2 };
         int[] array2 = { 1, 2, 3, 4, 5 };
         int[] array3 = { 1, 1, 1, 1, 1 };
-        int[] array4 = { 1 };
+        int[] array4 = { 0 };
 
         Console.WriteLine($"{IsArrayAscending(array1)} {IsArrayAscending(array2)}");
         Console.WriteLine($"{IsArrayDescending(array1)} {IsArrayDescending(array2)}");
@@ -17,11 +17,6 @@ internal class CheckSorting
 
     public static bool IsArrayAscending(int[] array)
     {
-        if (array.Length < 2)
-        {
-            return true;
-        }
-
         for (int i = 1; i < array.Length; i++)
         {
             if (array[i - 1] > array[i])
@@ -35,11 +30,6 @@ internal class CheckSorting
 
     public static bool IsArrayDescending(int[] array)
     {
-        if (array.Length < 2)
-        {
-            return true;
-        }
-
         for (int i = 1; i < array.Length; i++)
         {
             if (array[i - 1] < array[i])
